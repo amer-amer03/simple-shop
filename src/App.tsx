3bcd25340e38360e393f9b8703204bac90e5c053
Amer <a.atui@seclgroup.com>
2022-03-24 15:33:04 +0200

    code fix after review

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { FC } from 'react';
-import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import CatalogPage from './pages/CatalogPage';
-import Modal from './components/Modal';
-import HomePage from './pages/HomePage';
-import { modalSelector } from './store/selectors/modal';
-import Layout from './components/Layout';
-import './App.css';
+import { FC } from "react";
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import CatalogPage from "./pages/CatalogPage";
+import Modal from "./components/Modal";
+import HomePage from "./pages/HomePage";
+import { modalSelector } from "./store/selectors/modal";
+import Layout from "./components/Layout";
+import "./App.css";
 
 const App: FC = () => {
   const openModalType = useSelector(modalSelector);
+
+  const homePagePath = "/";
+  const catalogPagePath = "/catalog";
 
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path={homePagePath} element={<HomePage />} />
+          <Route path={catalogPagePath} element={<CatalogPage />} />
         </Routes>
 
         <Modal modalOpenType={openModalType} />
       </Layout>
-
     </div>
   );
-}
+};
 
 export default App;
